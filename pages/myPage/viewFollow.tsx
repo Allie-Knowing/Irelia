dbdbb992f60dc8ad86a0f8115592b9431a4f45bc
limyeonsang <limquo@gmail.com>
2022-07-04 17:11:52 +0900

    #8 Add scroll

--- pages/myPage/viewFollow.tsx
+++ pages/myPage/viewFollow.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-// import styled from "@emotion/styled";
+import styled from "@emotion/styled";
 import { UserItem, SelectButton } from "@views/viewFollow";
 
 const ViewFollowContainer: FC = () => {
@@ -44,22 +44,51 @@ const ViewFollowContainer: FC = () => {
       profile: "/a.png",
       name: "찌찌민",
     },
+    {
+      id: 9,
+      profile: "/a.png",
+      name: "찌찌민",
+    },
+    {
+      id: 10,
+      profile: "/a.png",
+      name: "찌찌민",
+    },
+    {
+      id: 11,
+      profile: "/a.png",
+      name: "찌찌민",
+    },
   ];
 
   return (
-    <div>
+    <Container>
       <SelectButton
         buttons={[
-          { name: "follower-list", onActive: () => {}, text: `팔로워${2}` },
-          { name: "following-list", onActive: () => {}, text: `팔로잉${100}` },
+          { name: "follower-list", onActive: () => {}, text: `팔로워 ${2}` },
+          { name: "following-list", onActive: () => {}, text: `팔로잉 ${100}` },
         ]}
         initalName="follower-list"
       />
-      {DUMMY.map((ele) => (
-        <UserItem key={ele.id} profile={ele.profile} name={ele.name} />
-      ))}
-    </div>
+
+      <UserItems>
+        {DUMMY.map((ele) => (
+          <UserItem key={ele.id} profile={ele.profile} name={ele.name} />
+        ))}
+      </UserItems>
+    </Container>
   );
 };
 
 export default ViewFollowContainer;
+
+const Container = styled.section`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const UserItems = styled.div`
+  flex: 1;
+  overflow: scroll;
+`;
