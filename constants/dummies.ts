a1f0220067cef8c5bdaac927a0e1be170f0fdaff
limyeonsang <limquo@gmail.com>
2022-07-07 08:43:12 +0900

    #10 Fix contants

---
+++ constants/dummies.ts
@@ -0,0 +1,52 @@
+import {
+  Animal,
+  Beauty,
+  Consulting,
+  Cook,
+  Fashion,
+  Game,
+  Health,
+  Music,
+  Sports,
+} from "@assets/images";
+
+const CONTENTS = [
+  {
+    name: "패션",
+    img: Fashion,
+  },
+  {
+    name: "헬스",
+    img: Health,
+  },
+  {
+    name: "동물",
+    img: Animal,
+  },
+  {
+    name: "뷰티",
+    img: Beauty,
+  },
+  {
+    name: "요리",
+    img: Cook,
+  },
+  {
+    name: "상담",
+    img: Consulting,
+  },
+  {
+    name: "게임",
+    img: Game,
+  },
+  {
+    name: "스포츠",
+    img: Sports,
+  },
+  {
+    name: "음악",
+    img: Music,
+  },
+];
+
+export default CONTENTS;
--- views/editInterests/ContentsLayout.tsx
+++ views/editInterests/ContentsLayout.tsx
@@ -1,56 +1,7 @@
 import styled from "@emotion/styled";
-import {
-  Animal,
-  Beauty,
-  Consulting,
-  Cook,
-  Fashion,
-  Game,
-  Health,
-  Music,
-  Sports,
-} from "@assets/images";
 import React, { FC, useEffect, useState } from "react";
 import { ContentsItem } from "@views/editInterests";
-
-const CONTENTS = [
-  {
-    name: "패션",
-    img: Fashion,
-  },
-  {
-    name: "헬스",
-    img: Health,
-  },
-  {
-    name: "동물",
-    img: Animal,
-  },
-  {
-    name: "뷰티",
-    img: Beauty,
-  },
-  {
-    name: "요리",
-    img: Cook,
-  },
-  {
-    name: "상담",
-    img: Consulting,
-  },
-  {
-    name: "게임",
-    img: Game,
-  },
-  {
-    name: "스포츠",
-    img: Sports,
-  },
-  {
-    name: "음악",
-    img: Music,
-  },
-];
+import CONTENTS from "@constants/dummies";
 
 interface PropsType {
   setButtonActive: React.Dispatch<React.SetStateAction<boolean>>;
