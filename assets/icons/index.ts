e48f6d233d6e22e0cc7c348414a7aa0c5abe7545
Kimjimin0807 <jm004471@naver.com>
2022-07-07 10:48:41 +0900

    #2 Add Iq Help icon

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as likeIcon } from "./like_icon.svg";
 export { default as playIcon } from "./play_icon.svg";
 export { default as logoIcon } from "./logo_icon.svg";
+export { default as iqHelpIcon } from "./iq_help_icon.svg";
