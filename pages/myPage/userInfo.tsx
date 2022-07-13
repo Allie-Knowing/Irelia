9a9596c83b9b9b3b197afcb123fdfb5144f6c87c
limyeonsang <limquo@gmail.com>
2022-07-13 22:54:40 +0900

    #12 Add userInfo ssr

--- pages/myPage/userInfo.tsx
+++ pages/myPage/userInfo.tsx
@@ -1,11 +1,27 @@
 import { UserInfo } from "@views/myPage";
 import styled from "@emotion/styled";
+import { GetServerSideProps } from "next";
+import { dehydrate, QueryClient } from "react-query";
+import queryKey from "@constants/queryKey";
+import { getMyInfo } from "@apis/myPage";
 
 const UserInfoContainer = () => (
   <Container>
     <UserInfo />
   </Container>
 );
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery([queryKey.myInfo], getMyInfo);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
 
 export default UserInfoContainer;
 
